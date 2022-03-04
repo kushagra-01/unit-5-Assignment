d31297a9866642ce2ca919698f8c143b855a1e4d
kushagra kamal <kushagrakamal98@gmail.com>
2022-03-04 19:08:03 +0530

    2 nd assignment

---
+++ unit-5/sprint-1/day-1/assignments/reactintro/my-app/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+    var name = ["apple","samsung","zoho","mail"]
+    var name2 = ["samsung","htc","micromax","ztc"]
+
+    return (
+        
+        <><div>
+            <h1>Mobile Operating</h1>
+            <ul>
+                {name.map((e) => (
+                    <li>{e}</li>
+                )
+
+                )}
+            </ul>
+
+        </div><div>
+                <h1>Manufacturer</h1>
+                <ul className='circle'>
+                    {name2.map((e) => {
+                        <li>{e}</li>
+                    }
+
+                    )}
+                </ul>
+
+            </div></>
+
+
+
+    )
+}
+
+
+export default App;
